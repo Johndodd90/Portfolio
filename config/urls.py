093ca49642d80badc4f5934f1e6e81c3b8ab876c
John Dodd <johndodd90@gmail.com>
2020-07-24 17:10:53 -0500

    Implement blog list page, add navbar link

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
     path("users/", include("portfolio.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path('blog/', include('portfolio.blog.urls', namespace='blog'),
+         ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
