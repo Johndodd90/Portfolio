6aafc7dfad56ed5ab81ceb3d433954dca4215199
John Dodd <johndodd90@gmail.com>
2020-07-26 13:31:10 -0500

    Add creator to Cheese model and custom migration

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+
 
 from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
@@ -8,6 +10,11 @@ class Blog(TimeStampedModel):
     title = models.CharField("Blog Title", max_length=255)
     slug = AutoSlugField("Blog Address", unique=True, always_update=False, populate_from="title")
     content = models.TextField("Content", blank=True)
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL
+    )
 
     def __str__(self):
         return self.title
